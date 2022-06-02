52c570ae31cd3e42145c8ecae02c12022b708ddb
justin <justinpak13@gmail.com>
2022-06-02 19:32:53 -0400

    minor bug fix

--- image.py
+++ image.py
@@ -5,12 +5,9 @@ from PIL import ImageDraw
 
 def convert_image(description):
     img = Image.open('images/image.jpg')
-    d1 = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype("GidoleFont/Gidole-Regular.ttf", 100)
-
-    d1.text((0, 0), description, fill=(255, 255, 255), font=font, stroke_width=5, stroke_fill=(0, 0, 0))
     img = img.resize((600,448))
-    img.save("images/image_text.jpg")
 
-    print(img.size)
+    d1 = ImageDraw.Draw(img)
+    font = ImageFont.truetype("GidoleFont/Gidole-Regular.ttf", 25)
+    d1.text((0, 0), description, fill=(255, 255, 255), font=font, stroke_width=2, stroke_fill=(0, 0, 0))
+    img.save("images/image_text.jpg")
